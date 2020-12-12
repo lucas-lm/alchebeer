e59e3c1dd7b1453a3d13d38de3b2df5a3750d288
Lucas Miranda <lucas-m@outlook.com>
2020-12-12 17:50:29 -0300

    feat: add links to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,26 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Button from '../components/Button'
 
 export default function Home() {
   return (
     <div className={styles.root}>
       <Head>
         <title>Alchebeer | Home</title>
-
+        <meta 
+          name="description" 
+          content="Crie, compartilhe e experimente novas bebidas"
+        />
       </Head>
+      <p>
+        We're here for the beer
+      </p>
+      <div className={styles.buttons}>
+        <Button to='/criar-receita'>Faça sua bebida</Button>
+        <Button to='/bebidas-da-galera  '>Bebidas da galera</Button>
+        <Button to='/nossas-receitas'>Nossas sugestões</Button>
+      </div>
 
-      We're here for the beer
     </div>
   )
 }
