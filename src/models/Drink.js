3c001ec6dd56aad8407620121e8d3a8a28c4f594
Lucas Miranda <lucas-m@outlook.com>
2020-12-12 18:34:36 -0300

    feat: create drink model

---
+++ src/models/Drink.js
@@ -0,0 +1,27 @@
+import { Schema } from 'mongoose'
+import { getModel } from '../utils'
+
+const DrinkSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  ingredients: {
+    type: Array,
+    required: true
+  },
+  creator: {
+    type: String,
+    required: true
+  }
+})
+
+const Drink = getModel('Drink', DrinkSchema)
+
+export default Drink
