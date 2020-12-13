513021a54e229a18c47e81ec75266ebc7b8f3579
Lucas Miranda <lucas-m@outlook.com>
2020-12-13 13:55:06 -0300

    feat: add new size to title component

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,7 +2,7 @@ import styles from './Title.module.css'
 
 const classFor = size => `${styles.root} ${styles[size]}`
 
-const Title = ({ size='regular', children, variant: Component ='p', ...rest }) => {
+const Title = ({ size='regular', children, variant: Component ='h1', ...rest }) => {
   return (
     <Component className={classFor(size)} {...rest}>{children}</Component>
   )
