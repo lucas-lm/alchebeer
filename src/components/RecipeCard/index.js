d62caf06785618f0680f987cfa090ed0e208d656
Lucas Miranda <lucas-m@outlook.com>
2020-12-13 13:54:26 -0300

    feat: add link to reicpe card

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -1,6 +1,7 @@
+import Link from 'next/link'
 import styles from './RecipeCard.module.css'
 
-const RecipeCard = ({ title, description, image, creator }) => {
+const RecipeCard = ({ title, description, image, creator, link }) => {
   return (
     <div className={styles.root}>
       <img src={image} className={styles.picture} />
@@ -8,6 +9,9 @@ const RecipeCard = ({ title, description, image, creator }) => {
         {creator && <small className={styles.creator}>Criado por: {creator}</small>}
         <h4 className={styles.title}>{title}</h4>
         {description && <p className={styles.description}>{description}</p>}
+        {link && <Link href={link}>
+          <a className={styles.link}>Ver mais</a>
+        </Link>}
       </div>
     </div>
   )
