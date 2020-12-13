b844deb39ebecfc74f4e42fe4911a4ed556e8ced
Lucas Miranda <lucas-m@outlook.com>
2020-12-12 23:15:14 -0300

    feat: add action variant

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,13 +1,23 @@
 import Link from 'next/link'
 import styles from './Button.module.css'
 
-const Button = ({ children, to }) => {
-  return (
-    <Link href={to}>
-      <a><button className={styles.root}>{children}</button></a>
-    </Link>
-    
-  )
+const Button = ({ children, to, variant='link', ...rest }) => {
+  switch (variant) {
+    case 'link':
+      return (
+        <Link href={to}>
+          <a><button className={styles.root}>{children}</button></a>
+        </Link>
+      )
+
+    case 'action':
+      return (
+          <button className={styles.root} {...rest}>{children}</button>
+      )
+  
+    default:
+      throw new Error('Invalid variant of button')
+  }
 }
 
 export default Button
