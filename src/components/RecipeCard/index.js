c3ef3eeca12d41f019c7b82cda12e30227c82706
Lucas Miranda <lucas-m@outlook.com>
2020-12-13 10:49:23 -0300

    feat: add creator field and styles

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -1,12 +1,13 @@
 import styles from './RecipeCard.module.css'
 
-const RecipeCard = ({ title, description, image }) => {
+const RecipeCard = ({ title, description, image, creator }) => {
   return (
     <div className={styles.root}>
       <img src={image} className={styles.picture} />
       <div className={styles['info-box']}>
-        <h4>{title}</h4>
-        <p>{description}</p>
+        {creator && <small className={styles.creator}>Criado por: {creator}</small>}
+        <h4 className={styles.title}>{title}</h4>
+        {description && <p className={styles.description}>{description}</p>}
       </div>
     </div>
   )
