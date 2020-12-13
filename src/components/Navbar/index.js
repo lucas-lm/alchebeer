e36e1db8d6cf10ca660f8cf9db6e03b2e37b332c
Lucas Miranda <lucas-m@outlook.com>
2020-12-13 09:03:11 -0300

    feat: add logo component to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,16 @@
 import styles from './Navbar.module.css'
 import Navlink from './Navlink'
+import Logo from '../Logo'
 
 const Navbar = () => {
   return (
     <header className={styles.root}>
-      <p>LOGO</p>
+      <Logo />
       <ul className={styles.menu}>
         <Navlink to='/'>Home</Navlink>
         <Navlink to='/criar-receita'>Criar Receita</Navlink>
         <Navlink to='/receitas-da-galera'>Receitas da Galera</Navlink>
         <Navlink to='/nossas-receitas'>Nossas Receitas</Navlink>
-        <Navlink to='/bebidas'>Bebidas</Navlink>
       </ul>
     </header>
   )
