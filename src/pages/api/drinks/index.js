0bbac81516baa880d358cc94599af1e4111d3c47
Lucas Miranda <lucas-m@outlook.com>
2020-12-13 09:04:48 -0300

    feat: add create drink

--- src/pages/api/drinks/index.js
+++ src/pages/api/drinks/index.js
@@ -5,8 +5,20 @@ dbConnect()
 
 export default async (req, res) => {
   const { method } = req
-  if (method === 'GET') {
-    const drinks = await Drink.find()
-    return res.json(drinks)
+  switch (method) {
+    case 'GET': {
+      const drinks = await Drink.find()
+      return res.json(drinks)
+    }
+      
+    case 'POST': {
+      const { body: { name, creator, ingredients } } = req
+      const drink = await Drink.create({name, creator, ingredients})
+      console.log(drink)
+      return res.json(drink)
+    }
+  
+    default:
+      return res.send('Something went wrong...')
   }
 }
